0b686dddd2d77690e967f95a40263494f185be2e
RupakSTHA <30586845+RupakSTHA@users.noreply.github.com>
2024-01-25 11:49:33 +0545

    Update gsap-from.js

--- gsap-from.js
+++ gsap-from.js
@@ -2,6 +2,7 @@ document.querySelector(".btn").addEventListener("click", ()=>{
     gsap.from(
         '.img1',
         {
+            //setting auto alpha 0 will makes opacity 0 to 1 and visibility show
             autoAlpha: 0,
             duration: 2
         }
